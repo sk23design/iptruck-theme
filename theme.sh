162cf354437ea708538b1203255c3d6dcd42c9c0
martin toner <martin@sk23.co.uk>
2026-05-12 11:33:41 +0100

    update

---
+++ theme.sh
@@ -0,0 +1,6 @@
+# shopify login --store tr-trading-uk.myshopify.com
+
+shopify theme dev --store iptruckparts.myshopify.com
+
+# set default shopify store
+# export SHOPIFY_CLI_STORE=tr-trading-uk.myshopify.com
