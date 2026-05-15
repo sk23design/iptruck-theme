fbc18499d627b7edb28d344b3d0baf55b809bf52
martin toner <martin@sk23.co.uk>
2026-05-15 11:15:03 +0100

    ignores

--- theme.sh
+++
@@ -1,6 +0,0 @@
-# shopify login --store tr-trading-uk.myshopify.com
-
-shopify theme dev --store iptruckparts.myshopify.com
-
-# set default shopify store
-# export SHOPIFY_CLI_STORE=tr-trading-uk.myshopify.com
